ac484ce35a4d087e6b7db3bd58c45bc25619894b
Ke Zhong <zhongke@google.com>
2021-07-20 21:37:07 +0000

    Add testdata file for client_test.go.

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -1,23 +1,52 @@
 package client
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 )
 
 const (
-	// Mocked response template, server could set queryName inside this response.
-	respTemplate = `{"data":{"%s":[{"Data":"{\"openconfig-module-catalog:name\": \"0\", \"openconfig-module-catalog:access\": {\"uri\": \"CPx\", \"md5-hash\": \"YC\"}, \"openconfig-module-catalog:prefix\": \"392Qv\", \"openconfig-module-catalog:summary\": \"e2W\", \"openconfig-module-catalog:version\": \"0\", \"openconfig-module-catalog:revision\": \"s\", \"openconfig-module-catalog:namespace\": \"V\", \"openconfig-module-catalog:submodules\": {\"submodule\": [{\"name\": \"1aX0v8\", \"access\": {\"uri\": \"63yc\", \"md5-hash\": \"wFJ3at\"}}, {\"name\": \"A94l\", \"access\": {\"uri\": \"LMSDG\", \"md5-hash\": \"fqF\"}}]}, \"openconfig-module-catalog:dependencies\": {\"required-module\": [\"rT\"]}, \"openconfig-module-catalog:classification\": {\"category\": \"openconfig-catalog-types:IETF_NETWORK_SERVICE\", \"subcategory\": \"openconfig-catalog-types:IETF_MODEL_TYPE\", \"deployment-status\": \"openconfig-catalog-types:EXPERIMENTAL\"}}"},{"Data":"{\"openconfig-module-catalog:name\": \"1\", \"openconfig-module-catalog:access\": {\"uri\": \"i\", \"md5-hash\": \"0DkF\"}, \"openconfig-module-catalog:prefix\": \"whAC\", \"openconfig-module-catalog:summary\": \"X5h\", \"openconfig-module-catalog:version\": \"1\", \"openconfig-module-catalog:revision\": \"OOq\", \"openconfig-module-catalog:namespace\": \"wnTaF\", \"openconfig-module-catalog:submodules\": {\"submodule\": [{\"name\": \"7R\", \"access\": {\"uri\": \"zV\", \"md5-hash\": \"DCdE\"}}, {\"name\": \"9E1\", \"access\": {\"uri\": \"lA0PP1\", \"md5-hash\": \"TLI\"}}, {\"name\": \"Cn94Q\", \"access\": {\"uri\": \"30nsE\", \"md5-hash\": \"zF5gm\"}}, {\"name\": \"de986\", \"access\": {\"uri\": \"KxGzK\", \"md5-hash\": \"JL\"}}]}, \"openconfig-module-catalog:dependencies\": {\"required-module\": [\"b\", \"iuaWU\"]}, \"openconfig-module-catalog:classification\": {\"category\": \"openconfig-catalog-types:IETF_MODEL_LAYER\", \"subcategory\": \"openconfig-catalog-types:IETF_TYPE_STANDARD\", \"deployment-status\": \"openconfig-catalog-types:PRODUCTION\"}}"},{"Data":"{\"openconfig-module-catalog:name\": \"2\", \"openconfig-module-catalog:access\": {\"uri\": \"xKbeLI\", \"md5-hash\": \"oep\"}, \"openconfig-module-catalog:prefix\": \"6gZ\", \"openconfig-module-catalog:summary\": \"3wb1lx\", \"openconfig-module-catalog:version\": \"2\", \"openconfig-module-catalog:revision\": \"zY82y\", \"openconfig-module-catalog:namespace\": \"E18EsV\", \"openconfig-module-catalog:submodules\": {\"submodule\": [{\"name\": \"26vi\", \"access\": {\"uri\": \"qMvvLw\", \"md5-hash\": \"TM7\"}}, {\"name\": \"Uf5Op\", \"access\": {\"uri\": \"mrXSm\", \"md5-hash\": \"5Gh\"}}]}, \"openconfig-module-catalog:dependencies\": {\"required-module\": [\"e5IN4\", \"b\", \"yD\"]}, \"openconfig-module-catalog:classification\": {\"category\": \"openconfig-catalog-types:IETF_NETWORK_ELEMENT\", \"subcategory\": \"openconfig-catalog-types:IETF_MODEL_TYPE\", \"deployment-status\": \"openconfig-catalog-types:EXPERIMENTAL\"}}"},{"Data":"{\"openconfig-module-catalog:name\": \"3\", \"openconfig-module-catalog:access\": {\"uri\": \"YZE\", \"md5-hash\": \"fE\"}, \"openconfig-module-catalog:prefix\": \"UWr\", \"openconfig-module-catalog:summary\": \"w3B\", \"openconfig-module-catalog:version\": \"3\", \"openconfig-module-catalog:revision\": \"zgCUn\", \"openconfig-module-catalog:namespace\": \"XAZ\", \"openconfig-module-catalog:submodules\": {\"submodule\": [{\"name\": \"pxN\", \"access\": {\"uri\": \"V65HM\", \"md5-hash\": \"Z\"}}]}, \"openconfig-module-catalog:dependencies\": {\"required-module\": [\"dDGxSc\", \"GM\", \"SllRk\", \"2\"]}, \"openconfig-module-catalog:classification\": {\"category\": \"openconfig-catalog-types:IETF_NETWORK_SERVICE\", \"subcategory\": \"openconfig-catalog-types:IETF_TYPE_VENDOR\", \"deployment-status\": \"openconfig-catalog-types:PRODUCTION\"}}"},{"Data":"{\"openconfig-module-catalog:name\": \"4\", \"openconfig-module-catalog:access\": {\"uri\": \"9c\", \"md5-hash\": \"ckdrUa\"}, \"openconfig-module-catalog:prefix\": \"I26\", \"openconfig-module-catalog:summary\": \"Axl\", \"openconfig-module-catalog:version\": \"4\", \"openconfig-module-catalog:revision\": \"0\", \"openconfig-module-catalog:namespace\": \"0D\", \"openconfig-module-catalog:submodules\": {\"submodule\": [{\"name\": \"nEa76V\", \"access\": {\"uri\": \"kv\", \"md5-hash\": \"vFF\"}}, {\"name\": \"x0O6Rb\", \"access\": {\"uri\": \"V1\", \"md5-hash\": \"XuUp\"}}]}, \"openconfig-module-catalog:dependencies\": {\"required-module\": [\"cKxRE\", \"WzvTc\"]}, \"openconfig-module-catalog:classification\": {\"category\": \"openconfig-catalog-types:IETF_NETWORK_SERVICE\", \"subcategory\": \"openconfig-catalog-types:IETF_TYPE_STANDARD\", \"deployment-status\": \"openconfig-catalog-types:EXPERIMENTAL\"}}"}]}}`
-	fieldName    = `Data`
+	// File name of testdata of mocked response template, server could set queryName inside this response.
+	testdataFile = `testdata`
+	// Name of filed containing raw JSON string of Module in response.
+	rawFieldName = `Data`
 )
 
-func setupHTTPTestServer() *httptest.Server {
+// ReadTestData reads testdata from file.
+func ReadTestData(filename string) (string, error) {
+	file, err := os.Open(testdataFile)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to open file: %v", err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var testdata string
+
+	// Append testdata line by line.
+	for scanner.Scan() {
+		testdata += scanner.Text()
+	}
+	return testdata, nil
+}
+
+// SetupHTTPTestServer sets up mock server for testing purpose.
+func SetupHTTPTestServer() (*httptest.Server, error) {
 	mux := http.NewServeMux()
 	ts := httptest.NewServer(mux)
-
+	respTemplate, err := ReadTestData(testdataFile)
+	if err != nil {
+		return nil, fmt.Errorf("Read test data failed: %v", err)
+	}
 	// Setup query handler functions.
 	mux.HandleFunc(`/query`, (func(w http.ResponseWriter, r *http.Request) {
 		if len(r.URL.Query()["query"]) == 1 {
@@ -34,13 +63,18 @@ func setupHTTPTestServer() *httptest.Server {
 
 	}))
 
-	return ts
+	return ts, nil
 }
 
 // TestQuery serves as a function to test all three functions inside client pkg,
 // including: (1) FormatHTTPQuery, (2) QueryServer, and (3) ParseModule.
 func TestQuery(t *testing.T) {
-	ts := setupHTTPTestServer()
+	ts, err := SetupHTTPTestServer()
+	if err != nil {
+		t.Errorf("Set up HTTP server failed: %v", err)
+		// Stop this test if server failed to launch.
+		return
+	}
 	defer ts.Close()
 
 	tests := []struct {
@@ -53,13 +87,13 @@ func TestQuery(t *testing.T) {
 			desc:            `Test query ModuleByOrgName`,
 			query:           `{ModulesByOrgName(OrgName:"1"){Data}}`,
 			queryName:       `ModulesByOrgName`,
-			wantModuleNames: []string{`0`, `1`, `2`, `3`, `4`},
+			wantModuleNames: []string{`0`, `1`, `2`},
 		},
 		{
 			desc:            `Test query ModuleByKey`,
 			query:           `{ModulesByOrgName(Name:"1"){Data}}`,
 			queryName:       `ModulesByOrgName`,
-			wantModuleNames: []string{`0`, `1`, `2`, `3`, `4`},
+			wantModuleNames: []string{`0`, `1`, `2`},
 		},
 	}
 	for _, tc := range tests {
@@ -72,7 +106,7 @@ func TestQuery(t *testing.T) {
 			if err != nil {
 				t.Errorf("Send query to server failed: %v", err)
 			}
-			modules, err := ParseModule(resp, fieldName, tc.queryName)
+			modules, err := ParseModule(resp, rawFieldName, tc.queryName)
 			if len(modules) != len(tc.wantModuleNames) {
 				t.Errorf("Parsed Modules length mismatch, get: %d, want: %d", len(modules), len(tc.wantModuleNames))
 			}
