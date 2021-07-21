8bfa82e7524e4731f12876666375345dc12b296a
Ke Zhong <zhongke@google.com>
2021-07-21 22:53:19 +0000

    Add const graphQL query and hostURL in demo client program.

--- demo/client/client.go
+++ demo/client/client.go
@@ -8,17 +8,24 @@ import (
 	"github.com/openconfig/catalog-server/pkg/client"
 )
 
+// const values for demo purposes.
+const (
+	rawFieldName = `Data`                                             // Name of field in response containing json data.
+	hostURL      = `https://helloworld-jpx33sh7ha-uc.a.run.app/query` // Address of deployed server.
+	graphQuery   = `{ModulesByOrgName(OrgName:"1"){Data}}`            // GraphQL query.
+	queryName    = `ModulesByOrgName`                                 // Name of graphQL query.
+)
+
 // Demo program to show how to use client library functions.
 func main() {
-	var queryPtr = flag.String("query", "", "query url")
 	var tokenPathPtr = flag.String("token", "", "file path of auth token")
-	var queryNamePtr = flag.String("queryName", "", "name of query")
-	var fieldNamePtr = flag.String("fieldName", "Data", "name of field in response containing json data")
-
 	flag.Parse()
 
+	// Format whole query URL.
+	queryURL := hostURL + `?query=` + graphQuery
+
 	// Format HTTP query.
-	req, err := client.FormatHTTPQuery(*queryPtr, *tokenPathPtr)
+	req, err := client.FormatHTTPQuery(queryURL, *tokenPathPtr)
 	if err != nil {
 		glog.Fatalf("catalog client: format query failed: %v", err)
 	}
@@ -30,7 +37,7 @@ func main() {
 	}
 
 	// Parse query results.
-	modules, err := client.ParseModule(resp, *fieldNamePtr, *queryNamePtr)
+	modules, err := client.ParseModule(resp, rawFieldName, queryName)
 	if err != nil {
 		glog.Fatalf("catalog client: parse response into go catalog modules failed: %v", err)
 	}
