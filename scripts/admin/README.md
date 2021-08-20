1763d04c370b71f5017e42e99eb1ad97b48a4cab
Ke Zhong <zhongke@google.com>
2021-08-20 16:06:10 +0000

    Add support for listing all existing users' access.

--- scripts/admin/README.md
+++ scripts/admin/README.md
@@ -1,5 +1,5 @@
 ### Overview
-This directory contains scrips for helping admin of catalog system. It provides functionality to:
+This directory contains scripts for helping admin of catalog system. It provides functionality to:
 + Delete existing account.
 + Grant write access of organizations to an existing account.
 
@@ -7,6 +7,8 @@ It does not provide functionalities for admin to register a new user as it can r
 via the login page by itself.
 
 ### Usage
++ To use these scripts the user must be admin of identity platform where the catalog system is deployed.
 + Admin needs to first run `bash create-ca.sh` to download a secret `KEY` to operate functionalities above.
 + To `delete`, run `go run deleteaccount.go -email EMAIL-OF-ACCOUNT -key PATH-TO-KEY`.
-+ TO `grant access`, run `go run grantaccess.go -email EMAIL-OF-ACCOUNT -key PATH-TO-KEY -claims STRING-OF_CLAIMS`. `STRING-OF_CLAIMS` is a string of list of organizations seperated by comma. That is, we expect the name of organization do not contain comma. Or we can choose a different delimiter with no conflicts with names of organizations.
++ TO `grant access`, run `go run grantaccess.go -email EMAIL-OF-ACCOUNT -key PATH-TO-KEY -claims STRING-OF_CLAIMS`. `STRING-OF_CLAIMS` is a string of list of organizations seperated by comma. That is, we expect the name of organization do not contain comma. Or we can choose a different delimiter with no conflicts with names of organizations. If user does not provide `-claims STRING-OF_CLAIMS` is equivalent to setting access of this account to `empty access` (i.e., no write access to any organization).
++ To `read all existing users' access`, run `go run grantacces.go -key PATH-TO-KEY -all`.
--- scripts/admin/grantaccess/grantaccess.go
+++ scripts/admin/grantaccess/grantaccess.go
@@ -7,6 +7,7 @@ import (
 	"log"
 
 	firebase "firebase.google.com/go/v4"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
@@ -18,11 +19,17 @@ const (
 func main() {
 	var keyPathPtr = flag.String("key", "", "file path of admin key")
 	var emailPtr = flag.String("email", "", "email account that you want to change access for")
-	var accessPtr = flag.String("access", "", "string of a list of organizations that account would be granted access to, seperated by delimiter")
+	var accessPtr = flag.String("access", "", "string of a list of organizations that account would be granted access to, seperated by delimiter. If not set, it means set empty access for this account")
+	var listall = flag.Bool("all", false, "whether to list all current users' claims")
+
 	flag.Parse()
 
-	if *keyPathPtr == "" || *emailPtr == "" || *accessPtr == "" {
-		log.Fatalf("Please provide all required inputs\n")
+	if *keyPathPtr == "" {
+		log.Fatalf("Please provide path to key file\n")
+	}
+
+	if *listall == false && *emailPtr == "" {
+		log.Fatalf("Please provide either provide email address or specify list `all`\n")
 	}
 
 	opt := option.WithCredentialsFile(*keyPathPtr)
@@ -40,27 +47,51 @@ func main() {
 		log.Fatalf("Generate firebase authentication admin failed\n")
 	}
 
-	// Fetch user via email.
-	user, err := client.GetUserByEmail(ctx, *emailPtr)
-	// Check whether this user exists or not.
-	if user == nil {
-		log.Fatalf("Account %s does not exist\n", *emailPtr)
-	}
+	// list all existing users and their claims
+	if *listall {
+		iter := client.Users(ctx, "")
+		for {
+			user, err := iter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				log.Fatalf("error listing users: %s\n", err)
+			}
+			// Assume all users have email addresses.
+			if user.CustomClaims == nil {
+				log.Printf("%s does not have claims\n", user.Email)
+			} else if access, ok := user.CustomClaims[accessField]; !ok {
+				log.Printf("%s does not have access\n", user.Email)
+			} else {
+				log.Printf("%s current access is: %v\n", user.Email, access)
+			}
+		}
+	} else {
+		// Fetch user via email.
+		user, err := client.GetUserByEmail(ctx, *emailPtr)
+		if err != nil {
+			log.Fatalf("Retrieve account by email failed: %v\n", err)
+		}
+		// Check whether this user exists or not.
+		if user == nil {
+			log.Fatalf("Account %s does not exist\n", *emailPtr)
+		}
 
-	// Obtain current claims from the user
-	currentClaims := user.CustomClaims
-	fmt.Println("Current claims", currentClaims)
-	// If the account does not contain any claims, then its claim is nil,
-	// we need to create a map of claims for it.
-	if currentClaims == nil {
-		currentClaims = make(map[string]interface{})
-	}
+		// Obtain current claims from the user
+		currentClaims := user.CustomClaims
+		fmt.Println("Current claims", currentClaims)
+		// If the account does not contain any claims, then its claim is nil,
+		// we need to create a map of claims for it.
+		if currentClaims == nil {
+			currentClaims = make(map[string]interface{})
+		}
 
-	// Set claims to grant access.
-	currentClaims[accessField] = *accessPtr
-	if err := client.SetCustomUserClaims(ctx, user.UID, currentClaims); err != nil {
-		log.Fatalln("error setting custom claims %v\n", err)
+		// Set claims to grant access.
+		currentClaims[accessField] = *accessPtr
+		if err := client.SetCustomUserClaims(ctx, user.UID, currentClaims); err != nil {
+			log.Fatalf("error setting custom claims %v\n", err)
+		}
+		fmt.Println("Successfully set up new claims", currentClaims)
 	}
-	fmt.Println("Successfully set up new claims", currentClaims)
-
 }
