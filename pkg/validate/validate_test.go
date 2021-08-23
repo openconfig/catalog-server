05930289a1f6d61a71e448823218b80b6ae0586c
Ke Zhong <zhongke@google.com>
2021-08-23 14:40:00 +0000

    Add tests for newly added functions.

--- pkg/validate/validate_test.go
+++ pkg/validate/validate_test.go
@@ -33,3 +33,32 @@ func TestValidateModule(t *testing.T) {
 		}
 	}
 }
+
+func TestValidateFeatureBundle(t *testing.T) {
+	tests := []struct {
+		input   string
+		wantErr bool
+	}{
+		{
+			input:   ``,
+			wantErr: true,
+		},
+		{
+			input:   `{}`,
+			wantErr: true,
+		},
+		{
+			input:   `{"openconfig-module-catalog:name": "", "openconfig-module-catalog:version": "version1"}`,
+			wantErr: true,
+		},
+		{
+			input:   `{"openconfig-module-catalog:name": "name1", "openconfig-module-catalog:version": "version1"}`,
+			wantErr: false,
+		},
+	}
+	for _, tc := range tests {
+		if _, err := ValidateFeatureBundle(tc.input); (err != nil) != tc.wantErr {
+			t.Errorf("ValidateFeatureBundle test with string failed, input: %s, wantErr: %t, err: %v", tc.input, tc.wantErr, err)
+		}
+	}
+}
