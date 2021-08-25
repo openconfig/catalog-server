77e0fcda0c998a9f38d53fba507498a4220dcd71
Ke Zhong <zhongke@google.com>
2021-08-25 22:35:43 +0000

    Add crawler tests.

---
+++ scripts/crawl/crawl_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	oc "github.com/openconfig/catalog-server/pkg/ygotgen"
+)
+
+func TestCrawlModules(t *testing.T) {
+	tests := []struct {
+		name    string
+		version string
+		summary string
+	}{
+		{
+			name:    "aug",
+			version: "1.2.0",
+			summary: "aug desc",
+		},
+		{
+			name:    "base",
+			version: "1.3.0",
+			summary: "base desc",
+		},
+		{
+			name:    "other",
+			version: "1.4.0",
+			summary: "other desc",
+		},
+		{
+			name:    "subdir1",
+			version: "1.5.0",
+			summary: "subdir1 desc",
+		},
+	}
+
+	paths := []string{"./models"}
+	// Dummy url
+	url := "https://github.com/openconfig/goyang/tree/master/testdata"
+	mods, names := crawlModules(paths, url)
+	var modules []*oc.OpenconfigModuleCatalog_Organizations_Organization_Modules_Module
+	for _, n := range names {
+		module := populateModule(mods[n], n)
+		if module == nil {
+			continue
+		}
+		modules = append(modules, module)
+	}
+
+	counter := 0
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("TestCrawlModules name: %s, version: %s, summary: %s", tc.name, tc.version, tc.summary), func(t *testing.T) {
+			if modules[counter].GetName() != tc.name || modules[counter].GetVersion() != tc.version || modules[counter].GetSummary() != tc.summary {
+				t.Errorf("crawled modules mismatch, name: %s, version: %s summary: %s, expected name: %s, expected version: %s, expected summary: %s", modules[counter].GetName(), modules[counter].GetVersion(), modules[counter].GetSummary(), tc.name, tc.version, tc.summary)
+			}
+			counter += 1
+		})
+	}
+}
